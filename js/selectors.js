f99d8c5383d25710435656b6f2c575b8770c0c64
Alex Lee <hyunho98@gmail.com>
2019-06-06 21:30:37 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,4 +1,12 @@
 // declare your functions here...
 function paragraphSelector() {
-  return $(`p`)
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
 }
